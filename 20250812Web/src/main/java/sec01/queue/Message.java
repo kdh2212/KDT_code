b99b68621074f6730efd6afff2f466b2655e1fc3
kdh2212 <jjun8248@gmail.com>
2025-08-12 13:59:53 +0900

    LIFO / FIFO 컬렉션 Queue 예제

---
+++ 20250812Web/src/main/java/sec01/queue/Message.java
@@ -0,0 +1,12 @@
+package sec01.queue;
+
+public class Message {
+	public String command;
+	public String to;
+	
+	public Message(String command, String to) {
+		this.command = command;
+		this.to = to;
+	}
+
+}
